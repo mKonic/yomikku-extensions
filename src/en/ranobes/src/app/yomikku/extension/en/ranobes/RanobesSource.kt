951d74c2a4161c62d239774343ff4b11108dafd2
mKonic <mkonic676@gmail.com>
2026-09-23 04:47:38 +0300

    ranobes: port the ranobes sites

---
+++ src/en/ranobes/src/app/yomikku/extension/en/ranobes/RanobesSource.kt
@@ -0,0 +1,5 @@
+package app.yomikku.extension.en.ranobes
+
+import app.yomikku.multisrc.ranobes.Ranobes
+
+class RanobesSource : Ranobes("Ranobes", "https://ranobes.top", "en", path = "novels")
---
+++ src/ru/ranobesru/src/app/yomikku/extension/ru/ranobesru/RanobesRU.kt
@@ -0,0 +1,5 @@
+package app.yomikku.extension.ru.ranobesru
+
+import app.yomikku.multisrc.ranobes.Ranobes
+
+class RanobesRU : Ranobes("Ranobes (RU)", "https://ranobes.com", "ru", path = "ranobe", postSearch = true)
