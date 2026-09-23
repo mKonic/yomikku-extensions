51a3b067fda1019268ce03074b79e44934ec9a48
mKonic <mkonic676@gmail.com>
2026-09-23 08:20:52 +0300

    divinedaolibrary: add Divine Dao Library on the fictioneer family

---
+++ src/en/divinedaolibrary/build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    id("yomikku.extension")
+}
+
+yomikku {
+    name = "Divine Dao Library"
+    className = ".DivineDaoLibrary"
+    versionCode = 1
+    theme("fictioneer")
+    source(name = "Divine Dao Library", lang = "en", baseUrl = "https://www.divinedaolibrary.com")
+}
---
+++ src/en/divinedaolibrary/src/app/yomikku/extension/en/divinedaolibrary/DivineDaoLibrary.kt
@@ -0,0 +1,6 @@
+package app.yomikku.extension.en.divinedaolibrary
+
+import app.yomikku.multisrc.fictioneer.Fictioneer
+
+/** Divine Dao Library, LNReader's divinedaolibrary plugin; the site has since moved to the Fictioneer theme. */
+class DivineDaoLibrary : Fictioneer("Divine Dao Library", "https://www.divinedaolibrary.com", "en", browsePage = "stories")
